cde91e2bcd0a84a396b9b154afee278a397753b1
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2012-11-04 18:38:45 -0500

    set up tests to run in eclipse

--- src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
+++ src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
@@ -2,7 +2,10 @@ package $package$
 
 import org.scalatra.test.scalatest._
 import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class $servlet_name$Tests extends ScalatraSuite with FunSuite {
 
   addServlet(classOf[$servlet_name$], "/*")
