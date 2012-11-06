18da4f945c051a100f0bc9bdff4bbfa76bd5b036
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2012-11-05 21:44:46 -0500

    revert the revert, relocate Database.scala, remove wonky unneeded file

--- src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
+++ src/main/g8/src/test/scala/$package$/$servlet_name$Tests.scala
@@ -1,7 +1,11 @@
 package $package$
 
 import org.scalatra.test.scalatest._
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class $servlet_name$Tests extends ScalatraSuite with FunSuite {
 
   addServlet(classOf[$servlet_name$], "/*")
